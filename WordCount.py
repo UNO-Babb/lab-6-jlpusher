f14eaac6029bf26b1b7234ca1b5c685c18a9eb03
jlpusher <jlpusher@unomaha.edu>
2025-03-17 02:46:59 +0000

    complete

--- WordCount.py
+++ WordCount.py
@@ -1,13 +1,26 @@
 #WordCount.py
-#Name:
-#Date:
-#Assignment:
+#Name: Jessica Pusher
+#Date: 3.16.25
+#Assignment: WordCount.py
 
 def main():
-  textFile = open("gettysberg.txt", 'r')
+  File = input("Enter a File Name: ") # tested with file names fish.txt and gettysberg.txt
+  textFile = open(File, 'r')
+  lineCount = 0
+  wordCount = 0
+  letterCount = 0
   
   for line in textFile:
-    print(line)
+    lineCount = lineCount + 1
+    words = line.split()
+    for w in words:
+      wordCount = wordCount + 1
+      letterCount = letterCount + wordCount +len(w) # I included wordcount since the spces between each word are also technically a character in the text.
+        
+    
+  print("Lines: ", lineCount)
+  print("Words: ", wordCount)
+  print("Characters: ", letterCount)
   
 
 if __name__ == '__main__':
